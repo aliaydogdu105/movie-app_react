d606eee5f0a408b8819a218ffedd99ed9570558b
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-09-06 04:39:21 +0300

    router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/detail/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
