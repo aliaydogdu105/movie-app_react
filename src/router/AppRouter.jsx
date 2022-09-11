f16bd556b762566abe608f80cb586f3ebcd57604
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-09-11 14:19:23 +0300

    movie-detail

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -13,7 +13,7 @@ const AppRouter = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/detail/:id" element={<MovieDetail />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
